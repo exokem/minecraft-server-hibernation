69b2c338a382c6ea23993b59fccf969812c22cd3
Exokem <ex010@live.com>
2025-06-26 20:57:19 -0400

    Timeout config value

--- lib/config/config.go
+++ lib/config/config.go
@@ -36,6 +36,8 @@ var (
 	ServHost      string = "127.0.0.1" // ServHost		is the ip address for msh to connect to minecraft server
 	ServPort      int                  // ServPort		is the port for msh to connect to minecraft server
 	ServPortQuery int                  // ServPortQuery	is the port for msh to perform stats query requests at minecraft server
+
+	ConnectTimeoutSeconds int = 10 // ConnectTimeoutSeconds is the duration, in seconds, after which pending connections to the server will be timed out
 )
 
 type Configuration struct {
@@ -198,6 +200,7 @@ func (c *Configuration) loadRuntime(confdef *Configuration) *errco.MshLog {
 	flag.IntVar(&ServPort, "servport", ServPort, "Specify the minecraft server port.")
 	flag.IntVar(&ServPortQuery, "servportquery", ServPortQuery, "Specify minecraft server port for queries.")
 	flag.BoolVar(&c.Msh.EnableQuery, "enablequery", c.Msh.EnableQuery, "Enables queries handling.")
+	flag.IntVar(&c.Msh.ConnectTimeoutSeconds, "connecttimeoutseconds", c.Msh.ConnectTimeoutSeconds, "Specify time to wait before timing out pending server connections.")
 	flag.Int64Var(&c.Msh.TimeBeforeStoppingEmptyServer, "timeout", c.Msh.TimeBeforeStoppingEmptyServer, "Specify time to wait before stopping minecraft server.")
 	flag.BoolVar(&c.Msh.SuspendAllow, "suspendallow", c.Msh.SuspendAllow, "Enables minecraft server process suspension.")
 	flag.IntVar(&c.Msh.SuspendRefresh, "suspendrefresh", c.Msh.SuspendRefresh, "Specify how often the suspended minecraft server process must be refreshed.")
